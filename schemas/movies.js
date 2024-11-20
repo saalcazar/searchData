ecf7595040ad42a6729350d21650041f08abd816
cabrerasam <alcazarcabreraalejandrosamuel@gmail.com>
2024-11-20 17:58:40 -0400

    Create validations andd CCRUD

---
+++ schemas/movies.js
@@ -0,0 +1,22 @@
+import z from 'zod'
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi'])
+  ),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(0),
+  poster: z.string().url({
+    message: 'poster mus be a valid url'
+  })
+})
+
+export function validateMovie (object) {
+  return movieSchema.safeParse(object)
+}
