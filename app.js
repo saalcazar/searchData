68a0f5efc95a737699f6dba920bee07ca63532e2
cabrerasam <alcazarcabreraalejandrosamuel@gmail.com>
2024-11-22 17:44:41 -0400

    Modify MVC for inyection of parameters MovieModel

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import express from 'express'
-import { moviesRouter } from './routes/movies.js'
+import { createMovieRouter } from './routes/movies.js'
 import { corsMiddleware } from './middlewares/cors.js'
 
-const PORT = process.env.PORT ?? 1234
-const app = express()
-app.disable('x-powered-by')
-app.use(express.json())
-app.use(corsMiddleware())
+export const createApp = ({ movieModel }) => {
+  const PORT = process.env.PORT ?? 1234
+  const app = express()
+  app.disable('x-powered-by')
+  app.use(express.json())
+  app.use(corsMiddleware())
 
-app.use('/movies', moviesRouter)
+  app.use('/movies', createMovieRouter({ movieModel }))
 
-app.use((req, res) => {
-  res.status(404).send('<h1>404</h1>')
-})
+  app.use((req, res) => {
+    res.status(404).send('<h1>404</h1>')
+  })
 
-app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`)
-})
+  app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+  })
+}
---
+++ serverPSQL.js
@@ -0,0 +1,3 @@
+import { createApp } from './app.js'
+import { MovieModel } from './models/database/movie.js'
+createApp({ movieModel: MovieModel })
