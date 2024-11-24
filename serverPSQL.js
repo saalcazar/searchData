8349e990a52eaaa26e72d700baa6fda0f269a14c
saalcazar <alcazar.samuel@gmail.com>
2024-11-24 04:37:28 -0400

    CRUD for areas, roles and regions

--- serverPSQL.js
+++ serverPSQL.js
@@ -2,4 +2,8 @@ import { createApp } from './app.js'
 import { AreaModel } from './models/users/area.js'
 import { RegionModel } from './models/users/region.js'
 import { RoleModel } from './models/users/role.js'
-createApp({ areaModel: AreaModel, regionModel: RegionModel, roleModel: RoleModel })
+createApp({
+  areaModel: AreaModel,
+  regionModel: RegionModel,
+  roleModel: RoleModel
+})
