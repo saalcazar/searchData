e1e6a7d5a1bcc21cfee7a8a2b3ba67e0784e4874
cabrerasam <alcazarcabreraalejandrosamuel@gmail.com>
2025-02-27 17:51:26 -0400

    CORS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,12 +2,12 @@ import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
   'http://localhost:5173',
-  'http://192.168.40.113:5173',
+  'http://192.168.40.110:5173',
   'http://192.168.50.101:5173',
   'http://192.168.0.16:5173',
   'http://192.168.0.16:5174',
   'http://localhost:1234',
-  'http://192.168.40.113:1234',
+  'http://192.168.40.110:1234',
   'http://192.168.50.101:1234',
   'http://192.168.0.16:1234',
   'https://searchdata.bo'
